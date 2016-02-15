32844b7e86b5146161d18d7ad1c6ac834736080a
Lavya R <lavya@qburst.com>
2016-02-15 10:15:48 +0530

    commiting

--- app/controllers/expenseController.js
+++ app/controllers/expenseController.js
@@ -1,15 +1,32 @@
 
-myApp.controller('expenseController', function($scope, $state){
-    ExpenseService.fetchExpenseDetails().then(function(data) {
+myApp.controller('expenseController', function($scope, $state, ExpenseDetailsService){
+    ExpenseDetailsService.fetchExpenseDetails().then(function(data) {
         $scope.data = data;
     })
     $scope.showAddExpense = true;
     $scope.Show_Add_form = function() {
-        $scope.firstname="";
-        $scope.lastname="";
-        $scope.add_email="";
+        $scope.date="";
+        $scope.purpose="";
+        $scope.pm="";
+        $scope.rate="";
         $('.add_error').hide();
         $('.shadow-div').addClass('display_block');
+        $scope.showAddExpense = $scope.showAddExpense ? false : true;
+    }
+    $scope.SubmitAdd = function() {
+        if ($scope.date == "" || $scope.purpose =="" || $scope.pm =="" || $scope.rate == "") {
+            $('.add_error').show();
+            $('.add_error').html('Enter all details..');
+            return false;
+        }else {
+            $scope.data.push({ 'date':$scope.date, 'purpose': $scope.purpose, 'pm':$scope.pm, 'rate':$scope.rate });
+            $('.shadow-div').removeClass('display_block');
+            $scope.showAddExpense = $scope.showAddExpense ? false : true;
+        }
+
+    }
+    $scope.HideAdd = function() {
+        $('.shadow-div').removeClass('display_block');
         $scope.showAddExpense = $scope.showAddExpense ? false : true;
     }
 	$( ".log_out" ).click(function() {
