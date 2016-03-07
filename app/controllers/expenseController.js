9f83876888fca9402a2fda7609d5a5962e50bf32
Lavya R <lavya@qburst.com>
2016-03-07 18:35:18 +0530

    Upload file completed

--- app/controllers/expenseController.js
+++ app/controllers/expenseController.js
@@ -1,4 +1,4 @@
-expenseManagementApp.controller('expenseController', function($scope, $state, $http, ExpenseDetailsService, $rootScope){
+expenseManagementApp.controller('expenseController', function($scope, $state, $http, ExpenseDetailsService, $rootScope, $timeout){
     $scope.showAddExpense = true;
 
     $scope.showAddForm = function() {
@@ -21,15 +21,15 @@ expenseManagementApp.controller('expenseController', function($scope, $state, $h
         } else {
             if( $rootScope.user == "lavya") {
                 if ($scope.pm== 'Nishin') {
-                    $rootScope.fullData.push({ 'date':$scope.date, 'purpose': $scope.purpose,'project':$scope.project, 'pm':$scope.pm, 'rate':$scope.rate, 'name' : $rootScope.user, 'status' : 0, 'invoice' : '$scope.invoice'});
+                    $rootScope.fullData.push({ 'date':$scope.date, 'purpose': $scope.purpose,'project':$scope.project, 'pm':$scope.pm, 'rate':$scope.rate, 'name' : $rootScope.user, 'status' : 0, 'invoice' : $scope.invoice});
                 }else {
-                    $rootScope.fullData.push({ 'date':$scope.date, 'purpose': $scope.purpose,'project':$scope.project, 'pm':$scope.pm, 'rate':$scope.rate, 'name' : $rootScope.user, 'status' : 1, 'invoice' : '$scope.invoice'});
+                    $rootScope.fullData.push({ 'date':$scope.date, 'purpose': $scope.purpose,'project':$scope.project, 'pm':$scope.pm, 'rate':$scope.rate, 'name' : $rootScope.user, 'status' : 1, 'invoice' : $scope.invoice});
                 }
             }else if( $rootScope.user == "vishnu") {
                 if ($scope.pm== 'Nishin') {
-                    $rootScope.fullData.push({ 'date':$scope.date, 'purpose': $scope.purpose,'project':$scope.project, 'pm':$scope.pm, 'rate':$scope.rate, 'name' : $rootScope.user, 'status' : 0, 'invoice' : '$scope.invoice'});
+                    $rootScope.fullData.push({ 'date':$scope.date, 'purpose': $scope.purpose,'project':$scope.project, 'pm':$scope.pm, 'rate':$scope.rate, 'name' : $rootScope.user, 'status' : 0, 'invoice' : $scope.invoice});
                 }else {
-                    $rootScope.fullData.push({ 'date':$scope.date, 'purpose': $scope.purpose,'project':$scope.project, 'pm':$scope.pm, 'rate':$scope.rate, 'name' : $rootScope.user, 'status' : 1, 'invoice' : '$scope.invoice'});
+                    $rootScope.fullData.push({ 'date':$scope.date, 'purpose': $scope.purpose,'project':$scope.project, 'pm':$scope.pm, 'rate':$scope.rate, 'name' : $rootScope.user, 'status' : 1, 'invoice' : $scope.invoice});
                 }
             }
             $('.shadow-div').removeClass('display_block');
@@ -40,15 +40,17 @@ expenseManagementApp.controller('expenseController', function($scope, $state, $h
         $('.shadow-div').removeClass('display_block');
         $scope.showAddExpense = $scope.showAddExpense ? false : true;
     }
-    $scope.readURL = function(element, index){
+    $scope.readURL = function( element, currentObj){
         if (element.files && element.files[0]) {
             var reader = new FileReader();
             
             reader.onload = function (e) {
                 $scope.invoice = e.target.result;
-                $scope.$apply(function() { 
-                    $rootScope.fullData[index].invoice = $scope.invoice;
-                });
+                    // $timeout(function(){
+                        $rootScope.$apply(function() {
+                            currentObj.invoice = $scope.invoice;
+                        });
+                // });
             }
         
         reader.readAsDataURL(element.files[0]);
@@ -56,7 +58,7 @@ expenseManagementApp.controller('expenseController', function($scope, $state, $h
       
         console.log($rootScope.fullData);
     }
-    $scope.openURL = function(index){
-        window.open($rootScope.fullData[index].invoice);   
+    $scope.openURL = function(curretObject){
+        window.open(curretObject.invoice);   
     }
 });
