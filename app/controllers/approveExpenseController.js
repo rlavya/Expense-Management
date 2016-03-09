2601e96b0c2d55d5535b242857254255cd411368
VISHNU <vishnuvijayasankar@qburst.com>
2016-03-09 14:32:43 +0530

    popup added

--- app/controllers/approveExpenseController.js
+++ app/controllers/approveExpenseController.js
@@ -1,5 +1,14 @@
 expenseManagementApp.controller('approveExpenseController', function($scope, $state, $http, $rootScope){
 	$scope.approve_check = function(index, name) {
-			$rootScope.fullData[index].status = 1;
+		$rootScope.fullData[index].status = 1;
+	}
+	$scope.popup = function() {
+		$(".popup").fadeIn(500);
+		$("#overlay").fadeIn(500);
+		$(".popup textarea").val('');
+	}
+	$scope.popupClose = function() {
+		$(".popup").fadeOut(500);
+		$("#overlay").fadeOut(500);
 	}
 });
