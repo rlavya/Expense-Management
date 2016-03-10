301e8a2c84f096cdf7d4aff68ae558cd120398ec
Lavya R <lavya@qburst.com>
2016-03-10 17:06:31 +0530

    note_feature

--- app/controllers/approveExpenseController.js
+++ app/controllers/approveExpenseController.js
@@ -1,5 +1,6 @@
 expenseManagementApp.controller('approveExpenseController', function($scope, $state, $http, $rootScope){
 	$scope.checkbox = [];
+	$scope.notePopup = true;
 	$scope.approve_check = function(index, event) {
 		if ($scope.checkbox[index] == true) {
 			$rootScope.fullData[index].status = 1;
@@ -8,19 +9,22 @@ expenseManagementApp.controller('approveExpenseController', function($scope, $st
 			$rootScope.fullData[index].status = 0;	
 		}
 	}
-	$scope.popup = function() {
-		$(".popup").fadeIn(500);
-		$("#overlay").fadeIn(500);
-		console.log($rootScope.fullData);
+	$scope.popup = function(index) {
+		debugger;
+		$('.overlay').addClass('display_block');
+		$scope.notePopup = $scope.notePopup ? false : true;
+		
 	}
 	$scope.popupClose = function() {
-		$(".popup").fadeOut(500);
-		$("#overlay").fadeOut(500);
+		$('.overlay').removeClass('display_block');
+		$scope.notePopup = $scope.notePopup ? false : true;
 	}
 	$scope.submitNote = function(index) {
-		$scope.fullData[index].note = $scope.note;
+		$scope.fullData[index].note = $scope.noteModel;
 		debugger
 		console.log($scope.fullData[index].note);
+		$('.overlay').removeClass('display_block');
+		$scope.notePopup = $scope.notePopup ? false : true;
 	}
 	$scope.openURL = function(curretObject){
         window.open(curretObject.invoice);   
